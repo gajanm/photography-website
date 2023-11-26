cd7cecff460ee47863c6244df51b22c5c9b3fc5e
gajanm <gajanmohanraj@gmail.com>
2023-11-26 11:59:04 -0500

    mobile interface

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -12,10 +12,10 @@ const Hero = () => {
   })
 
   return (
-    <div className='flex overflow-hidden text-white'>
+    <div className='md:flex overflow-hidden text-white '>
       <div className='w-1/2 h-screen flex flex-col justify-center items-center'>
         <h1 className="text-xl font-bold p-2 mb-1"> Hi, I am Naresh </h1>
-        <h3 className='md:text-3xl sm:text-2xl text-xl items-center text-center font-bold flex w-[75%] h-[25%] py-[100px]'>A New Jersey-based high schooler with a passion for photography and a desire to become a commercial photographer.</h3>
+        <h3 className='md:text-3xl sm:text-2xl text-lg items-center text-center font-bold flex w-[75%] h-[25%] py-[100px]'>A New Jersey-based high schooler with a passion for photography and a desire to become a commercial photographer.</h3>
         <div className='mt-4 hover:bg-black/5 rounded-full border-2 border-white'>
             <button className='p-6  '>
             <Link to='/portfolio' className="md:text-4xl sm:text-2xl text-xl font-bold p-1">View my work in  
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -26,8 +26,8 @@ const Navbar = () => {
         <div className={!nav ? 'fixed left-0 top-0 w-[80%] h-full border-r border-r-gray-900 bg-[#000000] ease-in-out duration-500' : 'fixed left-[-100%] ease-in-out duration-500'}>
           <h1 className='h-24 items-center justify-between px-16 p-4 w-[100%] text-3xl font-bold text-[#ffffff]'>Naresh Photography</h1>
           <ul className='uppercase pl-16 mt-16 text-white  '>
-            <li className='p-4 border-b border-gray-300'><Link to='/'> Home </Link></li>
-            <li className='p-4 border-b border-gray-300'><Link to='/portfolio'>Portfolio</Link></li>
+            <li className='p-4 '><Link to='/'> Home </Link></li>
+            <li className='p-4'><Link to='/portfolio'>Portfolio</Link></li>
             <li className='p-4'><Link to='/contact'>Contact</Link></li>
           </ul>
         </div>
