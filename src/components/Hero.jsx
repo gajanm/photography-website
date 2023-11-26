89cdd665b8ab71644975fa809229d518be279ca0
gajanm <gajanmohanraj@gmail.com>
2023-11-26 12:32:33 -0500

    mobile interface fixes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -12,12 +12,12 @@ const Hero = () => {
   })
 
   return (
-    <div className='md:flex overflow-hidden text-white '>
-      <div className='w-1/2 h-screen flex flex-col justify-center items-center'>
-        <h1 className="text-xl font-bold p-2 mb-1"> Hi, I am Naresh </h1>
-        <h3 className='md:text-3xl sm:text-2xl text-lg items-center text-center font-bold flex w-[75%] h-[25%] py-[100px]'>A New Jersey-based high schooler with a passion for photography and a desire to become a commercial photographer.</h3>
+    <div className='md:flex md:overflow-hidden text-white '>
+      <div className='w-full md:w-1/2 md:h-screen flex flex-col justify-center items-center'>
+        <h1 className="text-xl font-bold p-2 mb-1 mt-24 md:mt-0"> Hi, I am Naresh </h1>
+        <h3 className='md:text-3xl sm:text-2xl text-lg items-center text-center font-bold flex w-[75%] h-[25%] md:py-[100px]'>A New Jersey-based high schooler with a passion for photography and a desire to become a commercial photographer.</h3>
         <div className='mt-4 hover:bg-black/5 rounded-full border-2 border-white'>
-            <button className='p-6  '>
+            <button className='md:p-6 p-4  '>
             <Link to='/portfolio' className="md:text-4xl sm:text-2xl text-xl font-bold p-1">View my work in  
             <div>
             </div>
@@ -35,7 +35,7 @@ const Hero = () => {
         </div>
         
       </div>
-      <div className='w-1/2 pt-24 h-screen'> 
+      <div className='md:w-1/2 pt-24 md:h-screen'> 
         <img src={Naresh} alt='/'/>
       </div>
     </div>
